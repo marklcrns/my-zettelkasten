7513c150661505db871160a268d265ba4b5acf49
Mark Lucernas <marklcrns@gmail.com>
2021-08-23 12:03:14 -0700

    feat(3d-graph): Add node count as TextGeometry
    - Made title text dynamically generated
      - title text pulled from "#title-h1" html element innerHTMl
    - Node count is displayed next to title text
    - Improve code readability and reuse

--- static/3d-graph.js
+++ static/3d-graph.js
@@ -9,111 +9,6 @@ $.getJSON('../cache.json', function(data) {
 
   console.log(data.Graph);
 
-  function buildGraph(data) {
-    var graph = {};
-    var nodeMap = {};
-    var clusterCount = 0;
-
-    // Build nodes
-    var nodes = [];
-    Object.entries(data.vertices).forEach(([node,e]) => {
-      if (!excludedNodes.includes(e.ID)) {
-        var node = {}
-        // Meta
-        node.id = e.ID;
-        node.name = e.Title;
-        node.value = e.Slug;
-        node.date = new Date(e.Date).toDateString();
-        node.tags = e.Meta.tags;
-        node.clusterid = 0;
-        node.size = 0;
-        // For cross-linking
-        node.neighbors = [];
-        node.links = [];
-        // Push to nodeMap for ref
-        nodeMap[node.id] = nodes.length;
-
-        nodes.push(node);
-      }
-    })
-
-    // Build links
-    var links = [];
-    Object.entries(data.adjacencyMap).forEach(([source, entry]) => {
-      if (!$.isEmptyObject(entry)) {
-        Object.entries(entry).forEach((e) => {
-          var target = e[0];
-          var folgeType = e[1][0];
-
-          if (!excludedNodes.includes(source) && !excludedNodes.includes(target)) {
-            var link = {};
-            if (folgeType === "folgeinv") {
-              link.source = target;
-              link.target = source;
-            } else if (folgeType === "folge") {
-              link.source = source;
-              link.target = target;
-            } else {
-              return;   // equivalent to conventional `continue`
-            }
-
-            // Inherit clusterid from source node
-            if (nodes[nodeMap[link.source]].clusterid === 0) {
-              nodes[nodeMap[link.source]].clusterid = ++clusterCount;
-            }
-            nodes[nodeMap[link.target]].clusterid = nodes[nodeMap[link.source]].clusterid;
-
-            // Make source node bigger
-            nodes[nodeMap[link.source]].size += 1;
-
-            links.push(link);
-          }
-        });
-      }
-    })
-    graph.nodes = nodes;
-    graph.links = links;
-
-    // Cross-link node objects
-    graph.links.forEach(link => {
-      const a = graph.nodes[nodeMap[link.source]];
-      const b = graph.nodes[nodeMap[link.target]];
-      a.neighbors.push(b);
-      b.neighbors.push(a);
-      a.links.push(link);
-      b.links.push(link);
-    });
-
-    // // Random tree graph
-    // const N = 1000;
-    // graph = {
-      //   nodes: [...Array(N).keys()].map(i => ({
-        //     id: i,
-        //     name: "Chesca",
-        //     neighbors: [],
-        //     links: [],
-        //   })),
-      //   links: [...Array(N).keys()]
-      //   .filter(id => id)
-      //   .map(id => ({
-        //     source: id,
-        //     target: Math.round(Math.random() * (id-1))
-        //   }))
-      // };
-    //
-      // // Cross-link node objects
-    // graph.links.forEach(link => {
-      //   const a = graph.nodes[link.source];
-      //   const b = graph.nodes[link.target];
-      //   a.neighbors.push(b);
-      //   b.neighbors.push(a);
-      //   a.links.push(link);
-      //   b.links.push(link);
-      // });
-
-    return graph;
-  }
-
   const NODE_REL_SIZE = 3;
   const ON_CLICK_CAM_DISTANCE = 150;
   const FOCUS_TRANSITION_DURATION = 2500;
@@ -133,53 +28,6 @@ $.getJSON('../cache.json', function(data) {
 
   var lastNodeClick = 0;
   var lastBackgroundClick = 0;
-
-  // controls
-  const controls = { '3D Graph DAG': 'null'};
-  const gui = new dat.GUI();
-  gui.add(controls, '3D Graph DAG', [
-      'null',
-      'td',
-      'bu',
-      'lr',
-      'rl',
-      'zout',
-      'zin',
-      'radialout',
-      'radialin',
-      "null-index",
-      'td-index',
-      'bu-index',
-      'lr-index',
-      'rl-index',
-      'zout-index',
-      'zin-index',
-      'radialout-index',
-      'radialin-index',
-    ])
-    .onChange(orientation => {
-      if (orientation.includes('index')) {
-        const idx = excludedNodes.indexOf('index');
-        if (idx > -1) {
-          excludedNodes.splice(idx, 1);
-          Graph &&
-            Graph
-            .nodeAutoColorBy("size")
-            .graphData(buildGraph(data.Graph));
-        }
-        Graph && Graph.dagMode(orientation.replace('-index', ''));
-      } else {
-        if (!excludedNodes.includes('index')) {
-          excludedNodes.push('index');
-          Graph &&
-            Graph
-              .nodeAutoColorBy("clusterid")
-              .graphData(buildGraph(data.Graph));
-        }
-        Graph && Graph.dagMode(orientation)
-      }
-      console.log("excluded nodes: [" + excludedNodes + "]");
-    });
 
   // // Text-only nodes graph
   // const Graph = ForceGraph3D()
@@ -281,7 +129,7 @@ $.getJSON('../cache.json', function(data) {
 
       hoverNode = node || null;
 
-      updateHighlight();
+      updateHighlight(Graph);
     })
     .onLinkHover(link => {
       highlightNodes.clear();
@@ -293,16 +141,8 @@ $.getJSON('../cache.json', function(data) {
         highlightNodes.add(link.target);
       }
 
-      updateHighlight();
+      updateHighlight(Graph);
     });
-
-  function updateHighlight() {
-    // trigger update of highlighted objects in scene
-    Graph
-      .nodeColor(Graph.nodeColor())
-      .linkWidth(Graph.linkWidth())
-      .linkDirectionalParticles(Graph.linkDirectionalParticles());
-  }
 
   // Bloom Post-Processing effect
   const bloomPass = new UnrealBloomPass();
@@ -339,6 +179,7 @@ $.getJSON('../cache.json', function(data) {
     }
   }, 10);
 
+
   // Add HTML toggle buttons listeners
   document.getElementById('rotationToggle').addEventListener('click', event => {
     isCamRotationActive = !isCamRotationActive;
@@ -350,25 +191,227 @@ $.getJSON('../cache.json', function(data) {
     event.target.innerHTML = `${(isAnimationActive ? 'Pause' : 'Resume')} Animation`;
   });
 
+  addGUIDAGControls(Graph, data.Graph);
+  loadGraphZettelJumbotron(Graph);
+})
+
+function buildGraph(data) {
+  var graph = {};
+  var nodeMap = {};
+  var clusterCount = 0;
+
+  // Build nodes
+  var nodes = [];
+  Object.entries(data.vertices).forEach(([node,e]) => {
+    if (!excludedNodes.includes(e.ID)) {
+      var node = {}
+      // Meta
+      node.id = e.ID;
+      node.name = e.Title;
+      node.value = e.Slug;
+      node.date = new Date(e.Date).toDateString();
+      node.tags = e.Meta.tags;
+      node.clusterid = 0;
+      node.size = 0;
+      // For cross-linking
+      node.neighbors = [];
+      node.links = [];
+      // Push to nodeMap for ref
+      nodeMap[node.id] = nodes.length;
+
+      nodes.push(node);
+    }
+  })
+
+  // Build links
+  var links = [];
+  Object.entries(data.adjacencyMap).forEach(([source, entry]) => {
+    if (!$.isEmptyObject(entry)) {
+      Object.entries(entry).forEach((e) => {
+        var target = e[0];
+        var folgeType = e[1][0];
+
+        if (!excludedNodes.includes(source) && !excludedNodes.includes(target)) {
+          var link = {};
+          if (folgeType === "folgeinv") {
+            link.source = target;
+            link.target = source;
+          } else if (folgeType === "folge") {
+            link.source = source;
+            link.target = target;
+          } else {
+            return;   // equivalent to conventional `continue`
+          }
+
+          // Inherit clusterid from source node
+          if (nodes[nodeMap[link.source]].clusterid === 0) {
+            nodes[nodeMap[link.source]].clusterid = ++clusterCount;
+          }
+          nodes[nodeMap[link.target]].clusterid = nodes[nodeMap[link.source]].clusterid;
+
+          // Make source node bigger
+          nodes[nodeMap[link.source]].size += 1;
+
+          links.push(link);
+        }
+      });
+    }
+  })
+  graph.nodes = nodes;
+  graph.links = links;
+
+  // Cross-link node objects
+  graph.links.forEach(link => {
+    const a = graph.nodes[nodeMap[link.source]];
+    const b = graph.nodes[nodeMap[link.target]];
+    a.neighbors.push(b);
+    b.neighbors.push(a);
+    a.links.push(link);
+    b.links.push(link);
+  });
+
+  // // Random tree graph
+  // const N = 1000;
+  // graph = {
+    //   nodes: [...Array(N).keys()].map(i => ({
+      //     id: i,
+      //     name: "Chesca",
+      //     neighbors: [],
+      //     links: [],
+      //   })),
+    //   links: [...Array(N).keys()]
+    //   .filter(id => id)
+    //   .map(id => ({
+      //     source: id,
+      //     target: Math.round(Math.random() * (id-1))
+      //   }))
+    // };
+  //
+    // // Cross-link node objects
+  // graph.links.forEach(link => {
+    //   const a = graph.nodes[link.source];
+    //   const b = graph.nodes[link.target];
+    //   a.neighbors.push(b);
+    //   b.neighbors.push(a);
+    //   a.links.push(link);
+    //   b.links.push(link);
+    // });
+
+  return graph;
+}
+
+function loadGraphZettelJumbotron(graph) {
   const loader = new THREE.FontLoader();
   loader.load('./static/optimer_regular.typface.json', function(font) {
-    const geometry = new THREE.TextGeometry( 'INDEX', {
-      font: font,
-      size: 120,
-      height: 10,
-      curveSegments: 12,
-      bevelEnabled: true,
-      bevelThickness: 10,
-      bevelSize: 6,
-      bevelOffset: 0,
-      bevelSegments: 5
+
+    const titleGeometry = new THREE.TextGeometry(
+      document.getElementById('title-h1').innerHTML.toUpperCase(),
+      {
+        font: font,
+        size: 150,
+        height: 10,
+        curveSegments: 12,
+        bevelEnabled: true,
+        bevelThickness: 10,
+        bevelSize: 6,
+        bevelOffset: 0,
+        bevelSegments: 5
+      }
+    );
+    titleGeometry.computeBoundingSphere();
+    const titleMaterial = new THREE.MeshLambertMaterial({color: 0x5F5F5F, side: THREE.DoubleSide});
+    const titleMesh = new THREE.Mesh(titleGeometry, titleMaterial);
+    titleMesh.position.set(-titleGeometry.boundingSphere.radius, 100, -600); // (x, y, z)
+
+    const countGeometry = new THREE.TextGeometry(
+      'ct: ' +  graph.graphData().nodes.length.toString(),
+      {
+        font: font,
+        size: 50,
+        height: 10,
+        curveSegments: 12,
+        bevelEnabled: true,
+        bevelThickness: 8,
+        bevelSize: 4,
+        bevelOffset: 0,
+        bevelSegments: 5
+      }
+    );
+    countGeometry.computeBoundingSphere();
+    const countMaterial = new THREE.MeshLambertMaterial({color: 0xAF8700, side: THREE.DoubleSide});
+    const countMesh = new THREE.Mesh(countGeometry, countMaterial);
+    countMesh.position.set(
+      -titleMesh.position.x - countGeometry.boundingSphere.radius,  // x
+      titleMesh.position.y - countGeometry.boundingSphere.center.y,                                               // y
+      titleMesh.position.z + 50                                           // z
+    );
+
+    // Filter out existing TextGeometry objects
+    var filtered = graph.scene().children.filter(child => {
+      return !(child.type === "Mesh" && child.geometry.type === "TextGeometry");
     });
-    const material = new THREE.MeshLambertMaterial({color: 0x313131, side: THREE.DoubleSide});
+    graph.scene().children = filtered;
 
-    const mesh = new THREE.Mesh(geometry, material);
-    mesh.position.set(-200, 100, -500); // (x, y, z)
-
-    Graph.scene().add(mesh);
+    graph.scene().add(titleMesh);
+    graph.scene().add(countMesh);
   });
-})
+}
+
+function addGUIDAGControls(graph, data) {
+  const controls = { '3D Graph DAG': 'null'};
+  const gui = new dat.GUI();
+  gui.add(controls, '3D Graph DAG', [
+    'null',
+    'td',
+    'bu',
+    'lr',
+    'rl',
+    'zout',
+    'zin',
+    'radialout',
+    'radialin',
+    "null-index",
+    'td-index',
+    'bu-index',
+    'lr-index',
+    'rl-index',
+    'zout-index',
+    'zin-index',
+    'radialout-index',
+    'radialin-index',
+  ])
+    .onChange(orientation => {
+      if (orientation.includes('index')) {
+        const idx = excludedNodes.indexOf('index');
+        if (idx > -1) {
+          excludedNodes.splice(idx, 1);
+          graph &&
+            graph
+            .nodeAutoColorBy("size")
+            .graphData(buildGraph(data));
+          loadGraphZettelJumbotron(graph);
+        }
+        graph && graph.dagMode(orientation.replace('-index', ''));
+      } else {
+        if (!excludedNodes.includes('index')) {
+          excludedNodes.push('index');
+          graph &&
+            graph
+            .nodeAutoColorBy("clusterid")
+            .graphData(buildGraph(data));
+          loadGraphZettelJumbotron(graph);
+        }
+        graph && graph.dagMode(orientation)
+      }
+      console.log("excluded nodes: [" + excludedNodes + "]");
+    });
+}
+
+function updateHighlight(graph) {
+  // trigger update of highlighted objects in scene
+  graph
+    .nodeColor(graph.nodeColor())
+    .linkWidth(graph.linkWidth())
+    .linkDirectionalParticles(graph.linkDirectionalParticles());
+}
 
